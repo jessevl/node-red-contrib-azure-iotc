8752c663445929453638761cdf348759416655bb
Jesse van Leth <jessevanleth@outlook.com>
2019-07-23 11:38:32 +0200

    initial code commit

---
+++ azure-iotc-bridge.js
@@ -0,0 +1,28 @@
+module.exports = function(RED) {
+    const handleMessage = require('./lib/engine');
+
+    function AzureIotcBridgeNode(config) {
+        RED.nodes.createNode(this,config);
+        this.registrationHost = config.registrationHost;
+        this.sasToken = config.sasToken;
+        this.idScope = config.idScope;
+
+        var node = this;
+
+        node.on('input', function(msg) {
+            const parameters = {
+                idScope: node.idScope,
+                sasToken: node.sasToken,
+                registrationHost: node.registrationHost
+            };
+
+            try {
+                handleMessage({ ...parameters}, msg.payload.device, msg.payload.measurements, msg.payload.timestamp);
+            } catch (e) {
+                node.send('[ERROR]', e.message);
+            };
+            
+        });
+    }
+    RED.nodes.registerType("azure-iotc-bridge",AzureIotcBridgeNode);
+}
